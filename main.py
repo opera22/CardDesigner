52e8276f391cbe50f4619ea58c39a9aab6d819a1
Drew <andrewburns.r@gmail.com>
2020-10-09 16:07:59 -0600

    imported Pillow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
+
+from PIL import Image, ImageFont, ImageDraw
 
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
